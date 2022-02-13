922068f03d8543dcad599a2a4dbdbd3255bb9ab7
mpilon <matthew@mobiah.com>
2022-02-13 12:06:58 -0600

    Adding ssm agent to container

--- deploy/iac/terraform/prod/main.tf
+++ deploy/iac/terraform/prod/main.tf
@@ -67,7 +67,9 @@ module "prod-ftfp-task" {
                 ]
             }
         ]
-      },
+      }
+    ),
+    jsonencode(
       {
         "Version": "2012-10-17",
         "Statement": [
