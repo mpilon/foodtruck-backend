0fdaad5460cc9c183e7d5d48891d61f52bb9aad6
mpilon <matthew@mobiah.com>
2022-02-19 14:29:47 -0600

    Removing tmp fixes for broken pyenv path

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-export PIP_REQUIRE_VIRTUALENV=false
-
 eval "$(pyenv init -)"
 
 pyenv activate foodtruck
--- deploy/utils/exec.sh
+++ deploy/utils/exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ./exec.sh prod-ftfp eefb71211454457e87dd4a5ff201ffd1 prod-ftfp-api-container
+# ./exec.sh prod-ftfp 1bf6365852ba403a84719fd46e16bc52 prod-ftfp-api-container
 
 aws ecs execute-command --cluster $1 \
     --task $2 \
